cbebd53159e5b0872a73042c51fbc04e1efc86f1
Egor Dubrovin <dubrovin.en@ya.ru>
2023-02-16 23:19:11 +0300

    Добавлена поддержка корректного формта дат

--- index.py
+++ index.py
@@ -4,22 +4,31 @@ from base64 import b64decode
 from template import FillTemplate
 from mail import SendDocuments
 
+from dateutil.parser import parse
+
 def handler(event, context):
     if event['httpMethod'] != 'POST':
         return {
             'statusCode': 405
         }
 
-    print(f"DEBUG: original body is \n{event['body']}\n")
+    print(f"BODY: {event['body']}")
 
     body = b64decode(event['body']).decode("utf-8") if event['isBase64Encoded'] else event['body']
 
     form_answer = {}
     for row in body.split("\n\n"):
         key,val = row.split(":\n")
-        form_answer[f"<{key.upper()}>"] = val.removesuffix("\n")
+        val = val.removesuffix("\n")
+        
+        try:
+            val = parse(val).strftime(os.environ['DATE_FORMAT'])
+        except ValueError:
+            pass
+        
+        form_answer[f"<{key.upper()}>"] = val
     
-    print(f"DEBUG: form answers dict is \n{form_answer}\n")
+    print(f"ANSWERS: {form_answer}")
     
     mail_to = form_answer[os.environ['MAIL_TO_FORM_KEY']]
     documents = [
